546ccb8c7f7d53eb5665fc8f2817371d31ddd9c7
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-09-17 15:10:33 +0100

    #3 task(Auth): create AuthProvider with user state to connect to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { AuthProvider } from './services/auth';
 
 import HomePage from './pages/Home/home';
 import LoginPage from './pages/Login/login';
@@ -10,13 +10,15 @@ import SignUpPage from './pages/SignUp/signup';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component="HomePage" />
-        <Route exact path="/login" component="LoginPage" />
-        <Route exact path="/signup" component="SignUpPage" />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/signup" component={SignUpPage} />
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import app from '../../services/base';
 
 const LoginPage = () => (
   <>
     <h1>login Page</h1>
+    <button onClick={() => app.auth().signOut()}>Sign Out</button>
   </>
 );
 
