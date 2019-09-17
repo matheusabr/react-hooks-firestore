0fc2692aabc4cc1f66b266270b4c731ab583f49c
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-09-17 23:54:59 +0100

    #5 task(Login): create page + integrate firebase + redirect to main page

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,11 +1,59 @@
-import React from 'react';
-import app from '../../services/base';
+import React, { useCallback, useContext } from 'react';
+import { withRouter, Redirect } from 'react-router';
+import fireApp from '../../services/base';
+import { AuthContext } from '../../services/auth';
 
-const LoginPage = () => (
-  <>
-    <h1>login Page</h1>
-    <button onClick={() => app.auth().signOut()}>Sign Out</button>
-  </>
-);
+const LoginPage = ({ history }) => {
+  const handleLogin = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await fireApp
+          .auth()
+          .signInWithEmailAndPassword(email.value.toLowerCase(), password.value);
+        history.push('/');
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  );
 
-export default LoginPage;
+  // Check if user is already logged in
+  const { currentUser } = useContext(AuthContext)
+  if (currentUser) return <Redirect to='/' />
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          E-mail
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="user@email.com"
+          />
+        </label>
+        <br />
+        <label>
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+        </label>
+        <br />
+        <button type="submit">It's Play Time</button>
+        <br />
+        <button onClick={() => history.push('/signup')}>Sign Me Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default withRouter(LoginPage);
