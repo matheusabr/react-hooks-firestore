3e1af56bea9fdb6c9f0871953bc921302ca9c8b2
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-09-17 23:32:09 +0100

    #3 task(Routes): use private route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { AuthProvider } from './services/auth';
 
+import PrivateRoute from './routes/PrivateRoute';
+
 import HomePage from './pages/Home/home';
 import LoginPage from './pages/Login/login';
 import SignUpPage from './pages/SignUp/signup';
@@ -13,7 +15,7 @@ function App() {
     <AuthProvider>
       <Router>
         <div>
-          <Route exact path="/" component={HomePage} />
+          <PrivateRoute exact path="/" component={HomePage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/signup" component={SignUpPage} />
         </div>
